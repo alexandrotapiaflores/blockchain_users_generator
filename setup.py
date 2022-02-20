3248bea0defd802fca80108e875bb0914117f563
alexandrotapia591 <alexandrotapia.dev@gmail.com>
2022-02-20 14:05:11 -0500

    rename pgk to blockchain_users_generator

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name='users_generator',
+  name='blockchain_users_generator',
   version='0.1',
   license='MIT',
   author="Alexandro Tapia",
--- tests/example/test.py
+++ tests/example/test.py
@@ -1,5 +1,6 @@
-from src.users_generator import generator as users_generator
+from blockchain_users_generator import generator as blockchain_users_generator
+users  = blockchain_users_generator.generate(1000)
+users_dict = [user.to_dict() for user in users]
 
-user = users_generator.generate(1)
-
-print(user)
+print("\n\n------model------\n", users[0])
+print("\n\n------dict-------\n", users_dict[0])
