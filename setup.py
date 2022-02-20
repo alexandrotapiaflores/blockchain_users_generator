97ba3299d1838968f2a5a10971b5b76aa07aa8f2
alexandrotapia591 <alexandrotapia.dev@gmail.com>
2022-02-20 14:15:46 -0500

    ranem project link and version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
   name='blockchain_users_generator',
-  version='0.1',
+  version='0.1.1',
   license='MIT',
   author="Alexandro Tapia",
   author_email='alexandro@zpace.link',
   packages=find_packages('src'),
   package_dir={'': 'src'},
-  url='https://github.com/alexandrotapiaflores/users_generator',
+  url='https://github.com/alexandrotapiaflores/blockchain_users_generator',
   keywords='users generator blockchain',
   install_requires=[
     "asn1crypto==1.4.0",
